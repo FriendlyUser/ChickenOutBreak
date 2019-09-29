50dac8f36c795fca5e623ea3bea307c111678475
dli <davidli012345@gmail.com>
2019-09-28 18:08:26 -0700

    Adding Chicken OutBreak logic

--- main.cpp
+++ main.cpp
@@ -1,36 +1,31 @@
-#include <QApplication>
 #include <FelgoApplication>
-
+#include <QApplication>
 #include <QQmlApplicationEngine>
 
 
 int main(int argc, char *argv[])
 {
 
-    QApplication app(argc, argv);
+  QApplication app(argc, argv);
 
-    FelgoApplication felgo;
+  FelgoApplication felgo;
 
-    // QQmlApplicationEngine is the preferred way to start qml projects since Qt 5.2
-    // if you have older projects using Qt App wizards from previous QtCreator versions than 3.1, please change them to QQmlApplicationEngine
-    QQmlApplicationEngine engine;
-    felgo.initialize(&engine);
+  // QQmlApplicationEngine is the preferred way to start qml projects since Qt 5.2
+  // if you have older projects using Qt App wizards from previous QtCreator versions than 3.1, please change them to QQmlApplicationEngine
+  QQmlApplicationEngine engine;
+  felgo.initialize(&engine);
 
-    // Set an optional license key from project file
-    // This does not work if using Felgo Live, only for Felgo Cloud Builds and local builds
-    felgo.setLicenseKey(PRODUCT_LICENSE_KEY);
+  // use this instead of the below call for faster deployment on desktop when you comment the RESOURCES in the .pro file (this avoids compiling all your resources into the binary and has shorter compile times; also it allows re-running the same project in non-shadow builds)
+  // this is the preferred deployment option during development on Desktop (for deployment on mobile use the qrc approach below)
+  felgo.setMainQmlFileName(QStringLiteral("qml/ChickenOutbreakMain.qml"));
 
-    // use this during development
-    // for PUBLISHING, use the entry point below
-    felgo.setMainQmlFileName(QStringLiteral("qml/Main.qml"));
+  // use this instead of the above call to avoid deployment of the qml files and compile them into the binary with qt's resource system qrc
+  // this is the preferred deployment option for debug and release builds on mobile, and for publish builds on all platforms
+  // to avoid deployment of your qml files and images, also comment the DEPLOYMENTFOLDERS command in the .pro file
+  // only use the above non-qrc approach, during development on desktop
+//  felgo.setMainQmlFileName(QStringLiteral("qrc:/qml/ChickenOutbreakMain.qml"));
 
-    // use this instead of the above call to avoid deployment of the qml files and compile them into the binary with qt's resource system qrc
-    // this is the preferred deployment option for publishing games to the app stores, because then your qml files and js files are protected
-    // to avoid deployment of your qml files and images, also comment the DEPLOYMENTFOLDERS command in the .pro file
-    // also see the .pro file for more details
-    // felgo.setMainQmlFileName(QStringLiteral("qrc:/qml/Main.qml"));
+  engine.load(QUrl(felgo.mainQmlFileName()));
 
-    engine.load(QUrl(felgo.mainQmlFileName()));
-
-    return app.exec();
+  return app.exec();
 }
